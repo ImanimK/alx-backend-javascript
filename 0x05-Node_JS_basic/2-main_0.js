1efc189791b4cf5e0747aa9451abc904831ab824
ImanimK <138707585+ImanimK@users.noreply.github.com>
2024-11-26 19:38:04 +0300

    Update 2-main_0.js

--- 0x05-Node_JS_basic/2-main_0.js
+++ 0x05-Node_JS_basic/2-main_0.js
@@ -1,13 +1,4 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+  const countStudents = require('./2-read_file');
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
-});
+countStudents("nope.csv");
